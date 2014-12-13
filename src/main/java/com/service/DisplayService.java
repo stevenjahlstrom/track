3d8de5eb827b1d14b78355d73ade1197c8ce88a8
mortsahl <mortsahl@gmail.com>
2014-12-13 13:12:56 -0700

    after package change

--- src/main/java/com/service/DisplayService.java
+++
@@ -1,8 +0,0 @@
-package com.service;
-
-public interface DisplayService {
-
-    public void displayInventory();
-    public void displayHorses();
-    public void displayInventoryAndHorses();
-}
--- src/main/java/com/service/ProcessService.java
+++
@@ -1,6 +0,0 @@
-package com.service;
-
-public interface ProcessService {
-    public boolean processBet(String input);
-    public void processInput(String input);
-}
